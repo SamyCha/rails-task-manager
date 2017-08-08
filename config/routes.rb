ffe979a79b5ac726653ff7cf09376b2a2a3354c5
Samy Chalakani <samychalakani@gmail.com>
2017-08-08 15:23:49 +0200

    ajouter des taches

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
+  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # #lister les taches
+  # get 'tasks', to: 'tasks#index'
+
+  # #voir une tache
+  # get 'tasks/:id', to: 'tasks#show'
+
+  # #créer une tache
+  # get 'tasks/new', to: 'tasks#new'
+  # post 'tasks', to: 'tasks#create'
+
+  # #updater une tache
+  # get 'tasks/id/edit', to: 'tasks#edit'
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # #supprimer une tache
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
 end
